c9ee16e4eb949fb293b468e3da1ad6d42e9d4f5e
Arianna Foo <fooar@sheridancollege.ca>
2025-11-20 06:18:12 -0500

    Integrated Firebase with project

--- MovieMate/MovieMateApp.swift
+++ MovieMate/MovieMateApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+import Firebase
 
 @main
 struct MovieMateApp: App {
+    
+    // Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      
+      FirebaseApp.configure()
+      print("Configured Firebase!")
+    return true
+  }
 }
