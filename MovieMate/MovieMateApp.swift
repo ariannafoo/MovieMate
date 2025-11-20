7be4a2c9a860b023655545e22d86099f4491d18c
Arianna Foo <fooar@sheridancollege.ca>
2025-11-20 03:17:53 -0500

    Initial Commit

---
+++ MovieMate/MovieMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMateApp.swift
+//  MovieMate
+//
+//  Created by Arianna Foo on 2025-11-20.
+//
+
+import SwiftUI
+
+@main
+struct MovieMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
