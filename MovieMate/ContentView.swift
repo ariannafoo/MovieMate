a628f8111a84ae84858a912bb36824fd84d6d8e9
Arianna Foo <fooar@sheridancollege.ca>
2025-11-20 05:20:24 -0500

    Connecting all views

--- MovieMate/ContentView.swift
+++ MovieMate/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isLoggedIn = true // later: check Firebase user
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            if isLoggedIn {
+                // show main app
+                TabView {
+                    HomePageView()
+                        .tabItem {
+                            Label("Home", systemImage: "house.fill")
+                        }
+
+                    SettingsView()
+                        .tabItem {
+                            Label("Settings", systemImage: "gearshape.fill")
+                        }
+                }
+            } else {
+                // show auth screens
+                AuthView()
+            }
         }
-        .padding()
     }
 }
 
